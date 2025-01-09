fe13324227c560fb30ac7b3db0de5c9e319c91a3
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-01-09 15:30:45 +0000

    Add a flake to set this up
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = rec {
+          default = gitlab_due_date;
+
+          libgitlab = pkgs.stdenv.mkDerivation {
+            pname = "libgitlab";
+            version = "0.1.0";
+            src = ./subprojects/gitlab;
+            mesonBuildType = "release";
+            outputs = [
+              "dev"
+              "out"
+            ];
+            nativeBuildInputs = with pkgs; [
+              meson
+              ninja
+              pkg-config
+              cmake
+            ];
+            buildInputs = with pkgs; [
+              libcpr
+              nlohmann_json
+            ];
+          };
+
+          gitlab_due_date = pkgs.stdenv.mkDerivation {
+            pname = "gitlab_due_date";
+            version = "0.1.0";
+            src = ./.;
+            nativeBuildInputs = with pkgs; [
+              meson
+              ninja
+              pkg-config
+              cmake
+            ];
+
+            mesonBuildType = "release";
+
+            buildInputs =
+              with pkgs;
+              [
+                libcpr
+                spdlog
+                nlohmann_json
+              ]
+              ++ [ self.packages.${system}.libgitlab ];
+          };
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              meson
+              gcc
+              ninja
+              pkg-config
+              cmake
+              libcpr
+              nlohmann_json
+              spdlog
+            ];
+          };
+        };
+      }
+    );
+}
